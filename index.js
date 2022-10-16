8d97575be9075d22385f3be5cf82acf06fca7803
gregor murray <gregorian_rants@hotmail.com>
2022-10-16 12:08:54 +0100

    database configuration into its own module

--- index.js
+++ index.js
@@ -1,62 +1,48 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { response } = require("express");
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = "letmein12";
+const Note = require("./models/note.js");
+
+
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
@@ -64,42 +50,44 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
     date: new Date(),
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
 
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
